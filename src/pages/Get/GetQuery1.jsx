3c914e5a7ea78835ffce376bc5839f0192f4e87a
mohammadvaladbiegi2 <mohammadvaladbiegi2@gmail.com>
2024-07-26 17:12:25 +0330

    refetchInterval

--- src/pages/Get/GetQuery1.jsx
+++ src/pages/Get/GetQuery1.jsx
@@ -14,7 +14,8 @@ const {data,isLoading,error,isError} = useQuery({
    gcTime : 50000,   // catch Time
 staleTime : 10000, // white this options we say dont need fetch data in background until this time
 refetchOnMount: true, // if data be stale refetch agin data and if data be refresh dont fetch data until data be stale defult in this options is true if we select false data just one time fetch also can use always
-refetchOnWindowFocus : true // if data in DB change new data fetch wedout refresh page and default is true if set false even data change in DB just when refresh page refetch
+refetchOnWindowFocus : true, // if data in DB change new data fetch wedout refresh page and default is true if set false even data change in DB just when refresh page refetch
+// refetchInterval : 2000 // every 2 second reftech data and defuilt is false thise options more use for like price of coin like BTC 
 })
 
 if (isLoading) {
