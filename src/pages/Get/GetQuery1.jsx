ed33ce8ea1edf2efd2309aab341490fa6188dfc3
mohammadvaladbiegi2 <mohammadvaladbiegi2@gmail.com>
2024-07-26 17:17:50 +0330

    refetchIntervalInBackground

--- src/pages/Get/GetQuery1.jsx
+++ src/pages/Get/GetQuery1.jsx
@@ -16,6 +16,7 @@ staleTime : 10000, // white this options we say dont need fetch data in backgrou
 refetchOnMount: true, // if data be stale refetch agin data and if data be refresh dont fetch data until data be stale defult in this options is true if we select false data just one time fetch also can use always
 refetchOnWindowFocus : true, // if data in DB change new data fetch wedout refresh page and default is true if set false even data change in DB just when refresh page refetch
 // refetchInterval : 2000 // every 2 second reftech data and defuilt is false thise options more use for like price of coin like BTC 
+// refetchIntervalInBackground : true // default is false when set as true every 2 second refetch date even we not focuse in tab
 })
 
 if (isLoading) {
